cacecfbee66dd7424218112da754bfd6dd9fa56d
Khoa Le <khoatle165@gmail.com>
2019-08-01 04:35:25 +0200

    work on home page

---
+++ app.rb
@@ -0,0 +1,19 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path(__dir__)
+end
+
+require_relative "config/application"
+
+set :views, (proc { File.join(root, "app/views") })
+set :public_folder, File.dirname(__FILE__) + '/app/public'
+set :bind, '0.0.0.0'
+
+get '/' do
+  erb :index
+end
